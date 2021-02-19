ba102ab893ef39e4bf421283061f46ddac5b9d18
AnatolijDm <anat_dr25@rambler.ru>
2021-02-19 22:10:09 +0300

    feat: start create server

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const PORT = config.get('port') || 8080;
+
+const app = express();
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoURL'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        app.listen(PORT, () =>
+            console.log(`App is running on http://localhost:${PORT}`)
+        );
+    } catch (e) {
+        console.log('Server error', e.message);
+        process.exit(1);
+    }
+}
+
+start();
+
